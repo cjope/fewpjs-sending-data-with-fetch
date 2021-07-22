80330c996b67532eb87b1e7400096962a67322c2
Craig <craigjope@gmail.com>
2021-07-21 20:43:30 -0600

    commit tree a4922fd65883488338456f53eee6e45b32638a3b

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+
+let userUrl = "http://localhost:3000/users"
+
+
+let userInfo = {
+    name: "Steve",
+    email: "steve@steve.com"
+}
+
+function getID(obj) {
+    document.body.innerHTML = obj.id
+}
+
+let error = (alert) =>  document.body.innerHTML = alert.message
+
+let postRequest = {
+    method: "POST",
+    headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(userInfo)
+} 
+
+let submitData = () => {    
+     return fetch(userUrl, postRequest)
+        .then(function(resp) {
+            return resp.json()
+        })
+        .then(getID)
+        .catch(error)
+}
+
+
